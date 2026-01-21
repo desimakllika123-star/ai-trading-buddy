2714a57297f41464f40259798b7dfad4a39746c9
desimakllika123-star <desimakllika123@gmail.com>
2026-01-21 14:06:17 +0530

    Add main.dart with voice & demo TA

---
+++ lib/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+void main() {
+  runApp(const MyApp());
+}
+
+// ... (baaki poora code jo maine diya tha)
